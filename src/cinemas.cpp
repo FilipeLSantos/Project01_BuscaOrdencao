22d28999cfe48d60302cb7868985f7c9a3c8bf56
Filipe Santos <filipelopesdossantos85@gmail.com>
2025-05-30 14:38:40 -0300

    Atualização de arquivos em sala

---
+++ src/cinemas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class Cinema{
+    private: 
+
+    public: 
+
+        void titleType();
+        void genres();
+        void duration();
+
+
+};
